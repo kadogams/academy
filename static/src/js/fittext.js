b23ae64325ccef1148d8237bde2e20746cd75662
skadogam <kadogams@student.s19.be>
2020-07-06 21:06:12 +0200

    update js

--- static/src/js/fittext.js
+++ static/src/js/fittext.js
@@ -1,17 +1,20 @@
-// odoo.define('hello_world.main', function (require) {  const AbstractAction = require('web.AbstractAction');  console.log(AbstractAction);});
-
-odoo.define('fittext.main', function (require) {
-'use strict';
-
-var Dialog = require('web.Dialog');
-var publicWidget = require('web.public.widget');
-
-publicWidget.registry.HelloWorldPopup = publicWidget.Widget.extend({
-    selector: '#wrapwrap',
-
-    start: function () {
-        Dialog.alert(this, "Hello, world!");
-        return this._super.apply(this, arguments);
-    },
-})
+odoo.define('academy.fittext', function (require) {
+    console.log('AbstractAction');
+    $("#products_grid .description-hoverable").fitText();
 });
+
+// odoo.define('academy.main', function (require) {
+// 'use strict';
+//
+// var Dialog = require('web.Dialog');
+// var publicWidget = require('web.public.widget');
+//
+// publicWidget.registry.HelloWorldPopup = publicWidget.Widget.extend({
+//     selector: '#wrapwrap',
+//
+//     start: function () {
+//         Dialog.alert(this, "Hello, world!");
+//         return this._super.apply(this, arguments);
+//     },
+// })
+// });
